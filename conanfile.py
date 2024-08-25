a7833a18e519dcf769538de611e841cb95591a95
bebtio <bebtio@gmail.com>
2024-08-24 21:29:27 -0500

    Added shared object build type

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,18 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake
+from conan.tools.files import load
 
 class PngIORecipe(ConanFile):
+
+    name = "pngio"
+    url = "https://github.com/bebtio/pngIO"
+    version =  load("./","VERSION").strip()
+
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeToolchain", "CMakeDeps"
+
+    options = {"shared": [True,False]}
+    default_options = {"shared": True}
 
     # List of dependencies goes here.
     # One self.requires per dependency.
