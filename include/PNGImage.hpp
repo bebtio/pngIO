c25b4f774f0ef02872f14d3cb55e417bac8651c2
bebtio <bebtio@gmail.com>
2024-11-10 11:16:34 -0600

    Added function declarations for each pngImage generator type

--- include/PNGImage.hpp
+++ include/PNGImage.hpp
@@ -23,4 +23,29 @@ namespace pngImageUtil
 {
 pngIO::PixelVector 
 createPixelVectorFromChunks( std::vector<PNGChunk> chunks );
+
+
+    std::vector<pngIO::GrayscalePixel8>
+    createGrayScalPixel8Vector( const PNGChunk& chunk );
+
+    std::vector<pngIO::GrayscalePixel16>
+    createGrayscalePixel16Vector( const PNGChunk& chunk );
+
+    std::vector<pngIO::GrayscalePixelA8>
+    createGrayscalePixelA8Vector( const PNGChunk& chunk );
+
+    std::vector<pngIO::GrayscalePixelA16>
+    createGrayscalePixelA16Vector( const PNGChunk& chunk );
+    
+    std::vector<pngIO::RGBPixel8>
+    createRGBPixel8Vector( const PNGChunk& chunk );
+
+    std::vector<pngIO::RGBPixel16>
+    createRGBPixel16Vector( const PNGChunk& chunk );
+
+    std::vector<pngIO::RGBAPixel8>
+    createRGBAPixel8Vector( const PNGChunk& chunk );
+
+    std::vector<pngIO::RGBAPixel16>
+    createRGBAPixel16Vector( const PNGChunk& chunk );
 }
